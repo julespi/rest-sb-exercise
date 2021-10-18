f126b2dc2c7986eb7d0540388639481f143ed6d0
Julian Spinelli <julespi@gmail.com>
2021-10-18 19:15:50 -0300

    primer commit, agrego los archivos del proyecto

---
+++ src/main/java/com/julespi/restsbexercise/RestSbExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.julespi.restsbexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSbExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSbExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/julespi/restsbexercise/RestSbExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julespi.restsbexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSbExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
